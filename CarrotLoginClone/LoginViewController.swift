c8177827b8c11167f0911818aca7f6ba52842315
d-hye-n <qordkadkwla@gmail.com>
2025-05-07 10:28:31 +0900

    preview 추가 기본요소 추가

--- CarrotLoginClone/LoginViewController.swift
+++ CarrotLoginClone/LoginViewController.swift
@@ -4,11 +4,22 @@
 //
 //  Created by 김도현 on 4/30/25.
 //
+#Preview{
+    LoginViewController()
+}
 
 import UIKit
 import SnapKit
 
 final class LoginViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = "동네라서 가능한 모든 것\n 당근에서 가까운 이웃과 함께해요."
+        return label
+    }()
     
     private let button: UIButton = {
         let button = UIButton(type: .system)
@@ -31,10 +42,14 @@ final class LoginViewController: UIViewController {
             $0.centerY.equalTo(view.snp.centerY)
             
         }
-        
+        view.addSubview(label)
+        label.snp.makeConstraints {
+            $0.centerX.equalTo(view.snp.centerX)
+            $0.bottom.equalTo(button.snp.top).offset(-20)
+            
+        }
     }
   
-    
     @objc func moveToWelcome() {
         let viewController = WelcomeViewController()
         self.navigationController?.pushViewController(viewController, animated: true)
--- CarrotLoginClone/WelcomeViewController.swift
+++ CarrotLoginClone/WelcomeViewController.swift
@@ -4,10 +4,20 @@
 //
 //  Created by 김도현 on 4/30/25.
 //
+#Preview{
+    WelcomeViewController()
+}
 
 import UIKit
+import SnapKit
 
 final class WelcomeViewController: UIViewController {
+    
+    private let Image: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "Image")
+        return image
+    }()
     
     private let button: UIButton = {
         let button = UIButton(type: .system)
@@ -22,15 +32,20 @@ final class WelcomeViewController: UIViewController {
         view.backgroundColor = .white
         
         view.addSubview(button)
+        view.addSubview(Image)
         
         button.addTarget(self, action: #selector(moveToLogin), for: .touchUpInside)
         
         button.snp.makeConstraints {
             $0.centerX.equalTo(view.snp.centerX)
             $0.centerY.equalTo(view.snp.centerY)
-            
         }
         
+        Image.snp.makeConstraints {
+            $0.size.equalTo(150)
+            $0.centerX.equalTo(view.snp.centerX)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(88)
+        }
     }
     
     @objc func moveToLogin() {
